12a54fc4e00142d538e79e3266bddbd3849f3d67
V. Kuleshov <sajkunrnd@gmail.com>
2020-12-22 17:43:24 +0300

    gallery fix

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ class velesh_init_theme{
   public $font = '/assets/fonts/font.css';
 
   /* main script location  */
-  public $main_script= '/assets/script/main7.js';
+  public $main_script= '/assets/script/main8.js';
 
   /* main script slug */
   public $main_script_slug = 'theme-main-script-dev1';
