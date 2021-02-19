ce4a522af6ce11ab07c74ff124e417b6481a0a11
sajkun <sajkunrnd@gmail.com>
2021-02-19 16:03:26 +0300

    2.2.11

--- includes/class-content-output.php
+++ includes/class-content-output.php
@@ -344,6 +344,51 @@ class theme_content_output{
       $obj = get_queried_object();
 
       switch ($obj->post_type) {
+        case 'theme_campaign':
+          $all_clinic = get_posts(array(
+            'post_type' => 'theme_clinics',
+            'posts_per_page' => -1,
+          ));
+          $clinics = '';
+
+          foreach ($all_clinic as $t) {
+            $clinics .= sprintf('<option value="%1$s" >%1$s</option>', $t->post_title );
+          }
+
+
+          // $form_id = get_option('default_subscription_form');
+          $form_id = get_option('campaing_subscription_form_online');
+
+          $shortcode = sprintf('[wpforms id="%s"]',  $form_id);
+          $form    = do_shortcode($shortcode);
+          $form    = str_replace('%post_title%', $obj->post_title, $form );
+          $form  = str_replace('<option value="%theme_clinics%" >%theme_clinics%</option>', $clinics, $form );
+
+          $treatments = get_posts(array(
+            'posts_per_page' => -1,
+            'post_type' => 'theme_treatment'
+          ));
+          $options = '';
+
+          foreach ($treatments as $t) {
+            $options .= sprintf('<option value="%1$s" >%1$s</option>', $t->post_title);
+          }
+
+          $form = str_replace('<option value="%treatments%" >%treatments%</option>', $options, $form );
+
+          unset($wp_popup_forms[$form_id]);
+
+          $form_online = ($form_id)? str_replace('%single_treatment%', $obj->post_title, $form ): false;
+
+          $form_id   = get_option('campaing_subscription_form_inclinic');
+          $shortcode =  sprintf('[wpforms id="%s"]',  $form_id);
+          $form      = do_shortcode($shortcode);
+          $form      = str_replace('<option value="%treatments%" >%treatments%</option>', $options, $form );
+          $form      = str_replace('%post_title%', $obj->post_title, $form );
+          $form      = str_replace('<option value="%theme_clinics%" >%theme_clinics%</option>', $clinics, $form );
+          $form_inclicnic = ($form_id)? str_replace('%single_treatment%', $obj->post_title, $form ): false;
+
+          break;
         case 'theme_doctor':
           $first_name =  get_field('first_name', $obj->ID);
           $last_name  =  get_field('last_name', $obj->ID);
